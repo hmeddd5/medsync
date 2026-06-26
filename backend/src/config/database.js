fa6bcf91148e6c3219aaf6bcda3e6a78a3845862
Ahmad Errekab <Ahmaderrekab@gmail.com>
2026-06-26 19:19:58 -0400

    Finalisation du projet MedSync

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,6 +6,12 @@ const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     "postgres://medsync_user:medsync_password@postgres:5432/medsync_db",
+
+  ssl: process.env.DATABASE_URL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 export default pool;
